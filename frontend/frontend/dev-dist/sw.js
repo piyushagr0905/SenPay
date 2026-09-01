f0a5655deaaf4b51cae00859ea7a74ae80ae8877
piyushagr0905 <agarwallapiyush27@gmail.com>
2026-09-01 16:47:54 +0530

    Update local .env to production url

--- frontend/frontend/dev-dist/sw.js
+++ frontend/frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qae11c9ijh8"
+    "revision": "0.4kbnmlakhfg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
